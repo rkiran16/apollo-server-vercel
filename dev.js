ad3529112ed7f766624634e0b27ec71e7461aad0
Ravi Kanculakunta <ravikancula@gmail.com>
2023-09-03 09:26:58 -0700

    update dev.js

--- dev.js
+++ dev.js
@@ -2,4 +2,5 @@ import app from './api/index.js';
 import consola from 'consola';
 import dotenv from 'dotenv';
 dotenv.config();
-app.listen(3000, () => consola.info('Server started'));
+const port = 3000;
+app.listen(port, () => consola.info(`Server started ${port}`));
